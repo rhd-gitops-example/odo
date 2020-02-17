5eda3361aff5389ff85aa120dd880a438485d06a
chetan-rns <chetanrns1997@gmail.com>
2020-02-17 21:18:41 +0530

    Add functions to generate Tekton task objects
    - Added functions and test cases to create
    1) deploy-from-source-task
    2) deploy-using-kubectl-task
    3) github-status-task
    
    Co-Authored-By: Gagan Hegde <ghegde@redhat.com>

--- pkg/pipelines/tasks/tasks.go
+++ pkg/pipelines/tasks/tasks.go
@@ -3,19 +3,50 @@ package tasks
 import (
 	pipelinev1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
-
-	"github.com/openshift/odo/pkg/pipelines/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var (
-	taskTypeMeta = meta.TypeMeta("Task", "tekton.dev/v1alpha1")
-)
+// GenerateTasks will return a slice of tasks
+func GenerateTasks() []pipelinev1.Task {
+	return []pipelinev1.Task{
+		generateGithubStatusTask(),
+		generateDeployFromSourceTask(),
+		generateDeployUsingKubectlTask(),
+	}
+}
+
+func createTaskTypeMeta() metav1.TypeMeta {
+	return metav1.TypeMeta{
+		Kind:       "Task",
+		APIVersion: "tekton.dev/v1alpha1",
+	}
+}
+
+func createTaskObjectMeta(name string) metav1.ObjectMeta {
+	return metav1.ObjectMeta{
+		Name: name,
+	}
+}
 
 func createTaskResource(name string, resourceType string) pipelinev1.TaskResource {
 	return pipelinev1.TaskResource{
 		ResourceDeclaration: pipelinev1.ResourceDeclaration{
 			Name: name,
 			Type: resourceType,
+		},
+	}
+}
+
+func createEnvFromSecret(name string, localObjectReference string, key string) corev1.EnvVar {
+	return corev1.EnvVar{
+		Name: name,
+		ValueFrom: &corev1.EnvVarSource{
+			SecretKeyRef: &corev1.SecretKeySelector{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: localObjectReference,
+				},
+				Key: key,
+			},
 		},
 	}
 }
