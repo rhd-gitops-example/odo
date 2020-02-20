0e18162c7c7169c7d17c10304bc0138f342378d1
Kevin McDermott <kmcdermo@redhat.com>
2020-02-20 12:12:00 +0000

    Rework the new code around the base branch.
    Explicitly marshal the yaml to os.Stdout for now.
    
    Co-Authored-By: ishitasequeira <isequeir@redhat.com>

--- pkg/pipelines/bootstrap.go
+++ pkg/pipelines/bootstrap.go
@@ -3,6 +3,7 @@ package pipelines
 import (
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path"
 
@@ -16,8 +17,6 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 var (
 	dockerSecretName = "regcred"
 	saName           = "demo-sa"
@@ -39,18 +38,6 @@ var (
 	}
 )
 
-// DefaultTokenFileName is the default token file name
-var DefaultTokenFileName string = "~/Downloads/token/token.txt"
-
-// DefaultQuayIOAuthFileName is the default auth file name
-var DefaultQuayIOAuthFileName string = "~/Downloads/<username>-auth.json"
-=======
-var DefaultTokenFilename string = "~/Downloads/token"
-var DefaultQuayIOAuthFilename string = "~/Downloads/<username>-auth.json"
->>>>>>> de06981f0... made the requested changes mentioned in the PR
-
-=======
->>>>>>> d8c543602... created 4 required-flags quay-username, github-token, dockerconfigjson,base-repository
 // BootstrapOptions is a struct that provides the optional flags
 type BootstrapOptions struct {
 	Prefix string
@@ -59,7 +46,6 @@ type BootstrapOptions struct {
 // Bootstrap is the main driver for getting OpenShift pipelines for GitOps
 // configured with a basic configuration.
 func Bootstrap(quayUsername, baseRepo, githubToken, quayIOAuthFilename string, o *BootstrapOptions) error {
-
 	// First, check for Tekton.  We proceed only if Tekton is installed
 	installed, err := checkTektonInstall()
 	if err != nil {
@@ -70,55 +56,17 @@ func Bootstrap(quayUsername, baseRepo, githubToken, quayIOAuthFilename string, o
 	}
 
 	outputs := make([]interface{}, 0)
-<<<<<<< HEAD
-	//  Create GitHub Secret
-	githubAuth, err := createGithubSecret()
-	tokenPath, err := getTokenFileName(o)
-=======
 
-<<<<<<< HEAD
-	tokenPath, err := homedir.Expand(o.TokenFilename)
->>>>>>> 875ecd79c... made the requested changes mentioned in the PR and removed the unwanted comments
+	githubAuth, err := createOpaqueSecret("github-auth", githubToken)
 	if err != nil {
-		return fmt.Errorf("failed to generate token path to file: %w", err)
-	}
-	f, err := os.Open(tokenPath)
-	if err != nil {
-<<<<<<< HEAD
-		return fmt.Errorf("failed to open path to TokenFileName: %w", err)
-=======
-		return fmt.Errorf("failed to expand the path %s: %w", authJSONPath, err)
->>>>>>> 424497f3b... made the required changes mentioned in the PR
+		return err
 	}
 	outputs = append(outputs, githubAuth)
 
 	// Create Docker Secret
 	dockerSecret, err := createDockerSecret(quayUsername)
-=======
-	githubAuth, err := createOpaqueSecret("github-auth", githubToken)
->>>>>>> d8c543602... created 4 required-flags quay-username, github-token, dockerconfigjson,base-repository
 	if err != nil {
-<<<<<<< HEAD
 		return err
-=======
-		return fmt.Errorf("failed to expand the path %s: %w", authJSONPath, err)
->>>>>>> 424497f3b... made the required changes mentioned in the PR
-	}
-	if o.QuayIOAuthFileName == "~/Downloads/<username>-auth.json" {
-		authJSONPath, err := pathToDownloadedFile(quayUsername + "-auth.json")
-		if err != nil {
-			return fmt.Errorf("failed to generate path to file: %w", err)
-		}
-
-		f, err = os.Open(authJSONPath)
-		if err != nil {
-			return err
-		}
-	} else {
-		f, err = os.Open(o.QuayIOAuthFileName)
-		if err != nil {
-			return err
-		}
 	}
 	outputs = append(outputs, dockerSecret)
 
@@ -141,47 +89,7 @@ func Bootstrap(quayUsername, baseRepo, githubToken, quayIOAuthFilename string, o
 	outputs = append(outputs, createRoleBinding(roleBindingName, &sa, role.Kind, role.Name))
 	outputs = append(outputs, createRoleBinding("edit-clusterrole-binding", &sa, "ClusterRole", "edit"))
 
-	// Marshall
-	for _, r := range outputs {
-		data, err := yaml.Marshal(r)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("%s---\n", data)
-	}
-
-	return nil
-}
-
-// createGithubSecret creates Github secret
-func createGithubSecret() (*corev1.Secret, error) {
-	tokenPath, err := pathToDownloadedFile("token")
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate path to file: %w", err)
-	}
-	f, err := os.Open(tokenPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read token file %s due to %w", tokenPath, err)
-	}
-	outputs = append(outputs, githubAuth)
-
-	authJSONPath, err := homedir.Expand(quayIOAuthFilename)
-	if err != nil {
-		return fmt.Errorf("failed to generate path to file: %w", err)
-	}
-
-	f, err := os.Open(authJSONPath)
-	if err != nil {
-		return fmt.Errorf("failed to open path authJSON : %w", err)
-	}
-	defer f.Close()
-
-	githubAuth, err := createOpaqueSecret("github-auth", f)
-	if err != nil {
-		return nil, err
-	}
-
-	return githubAuth, nil
+	return marshalOutputs(os.Stdout, outputs)
 }
 
 // createDockerSecret creates Docker secret
@@ -201,9 +109,10 @@ func createDockerSecret(quayUsername string) (*corev1.Secret, error) {
 	if err != nil {
 		return nil, err
 	}
-	return dockerSecret, nil
-}
 
+	return dockerSecret, nil
+
+}
 func pathToDownloadedFile(fname string) (string, error) {
 	return homedir.Expand(path.Join("~/Downloads/", fname))
 }
@@ -215,4 +124,18 @@ func checkTektonInstall() (bool, error) {
 		return false, err
 	}
 	return tektonChecker.checkInstall()
+}
+
+func marshalOutputs(out io.Writer, outputs []interface{}) error {
+	for _, r := range outputs {
+		data, err := yaml.Marshal(r)
+		if err != nil {
+			return fmt.Errorf("failed to marshal data: %w", err)
+		}
+		_, err = fmt.Fprintf(out, "%s---\n", data)
+		if err != nil {
+			return fmt.Errorf("failed to write data: %w", err)
+		}
+	}
+	return nil
 }
