2e439d62e93dbb302c5b539d449535b755799439
chetan-rns <chetanrns1997@gmail.com>
2020-02-25 10:43:21 +0530

    Add function to validate image registry URL

--- pkg/pipelines/bootstrap_test.go
+++ pkg/pipelines/bootstrap_test.go
@@ -1,1 +1,41 @@
 package pipelines
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestValidateImageRepo(t *testing.T) {
+	tests := []struct {
+		description string
+		URL         string
+		validURL    string
+		isValid     bool
+	}{
+		{
+			"Valid image regsitry URL",
+			"quay.io/sample-user/sample-repo",
+			"quay.io/sample-user/sample-repo",
+			true,
+		},
+		{
+			"Invalid image registry URL with missing repo name",
+			"quay.io/sample-user",
+			"quay.io/sample-user/sample-repo",
+			false,
+		},
+		{
+			"Invalid image registry URL with whitespaces",
+			"quay.io/sample-user/ ",
+			"quay.io/sample-user/sample-repo",
+			false,
+		},
+	}
+	for _, test := range tests {
+		isValid, _ := validateImageRepo(test.URL)
+		if diff := cmp.Diff(isValid, test.isValid); diff != "" {
+			t.Fatalf("validateImageRepo() failed:\n%s", diff)
+		}
+	}
+}
