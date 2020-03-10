a3ea6e2e8b12007746858c4b99bc630964604c52
William Tam <email.wtam@gmail.com>
2020-03-10 11:03:44 -0400

    Add new rolebinding to outputs

--- pkg/pipelines/bootstrap.go
+++ pkg/pipelines/bootstrap.go
@@ -167,10 +167,10 @@ func createManifestsForImageRepo(sa *corev1.ServiceAccount, isInternalRegistry b
 		out = append(out, createRoleBinding(meta.NamespacedName(internalRegistryNamespace, "internal-registry-binding"), sa, "ClusterRole", "edit"))
 
 		// add image puller role to allow pulling app images across namespaces from dev and stage envirnments
-		createRoleBindingForSubjects(meta.NamespacedName(internalRegistryNamespace, "image-puller-binding"), "ClusterRole", "system:image-puller",
+		out = append(out, createRoleBindingForSubjects(meta.NamespacedName(internalRegistryNamespace, "image-puller-binding"), "ClusterRole", "system:image-puller",
 			[]v1rbac.Subject{v1rbac.Subject{Kind: "ServiceAccount", Name: "default", Namespace: namespaces["dev"]},
 				v1rbac.Subject{Kind: "ServiceAccount", Name: "default", Namespace: namespaces["stage"]},
-			})
+			}))
 
 	} else {
 		// Add secret to service account if external registry is used
