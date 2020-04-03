cb1bbb0d949600abf402df8a275a7a5ab63ce435
ishitasequeira <isequeir@redhat.com>
2020-04-03 14:12:45 -0400

    added test cases for environment command

---
+++ pkg/odo/cli/pipelines/environment_test.go
@@ -0,0 +1,59 @@
+package pipelines
+
+import (
+	"testing"
+
+	"github.com/spf13/cobra"
+)
+
+func TestCompleteEnvParameters(t *testing.T) {
+	completeTests := []struct {
+		name       string
+		prefix     string
+		wantPrefix string
+	}{
+		{"no prefix", "", ""},
+		{"prefix with hyphen", "test-", "test-"},
+		{"prefix without hyphen", "test", "test-"},
+	}
+
+	for _, tt := range completeTests {
+		o := EnvParameters{prefix: tt.prefix}
+
+		err := o.Complete("test", &cobra.Command{}, []string{"test", "test/repo"})
+
+		if err != nil {
+			t.Errorf("Complete() %#v failed: ", err)
+		}
+
+		if o.prefix != tt.wantPrefix {
+			t.Errorf("Complete() %#v prefix: got %s, want %s", tt.name, o.prefix, tt.wantPrefix)
+		}
+	}
+}
+
+func TestValidateEnvParameters(t *testing.T) {
+	optionTests := []struct {
+		name    string
+		gitRepo string
+		errMsg  string
+	}{
+		{"invalid repo", "test", "repo must be org/repo"},
+		{"valid repo", "test/repo", ""},
+	}
+
+	for _, tt := range optionTests {
+		o := EnvParameters{gitOpsRepo: tt.gitRepo, prefix: "test"}
+
+		err := o.Validate()
+
+		if err != nil && tt.errMsg == "" {
+			t.Errorf("Validate() %#v got an unexpected error: %s", tt.name, err)
+			continue
+		}
+
+		if !matchError(t, tt.errMsg, err) {
+			t.Errorf("Validate() %#v failed to match error: got %s, want %s", tt.name, err, tt.errMsg)
+		}
+	}
+}
