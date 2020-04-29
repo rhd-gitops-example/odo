7c8368f9e6a51997ff682a453d19521ec98d9d54
William Tam <email.wtam@gmail.com>
2020-04-29 10:00:17 -0400

    Add get secret method

--- pkg/manifest/webhook/webhook_resources.go
+++ pkg/manifest/webhook/webhook_resources.go
@@ -3,11 +3,14 @@ package webhook
 import (
 	routeclientset "github.com/openshift/client-go/route/clientset/versioned/typed/route/v1"
 	"github.com/openshift/odo/pkg/manifest/clientconfig"
+	"github.com/pkg/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 type webhookResources struct {
 	routeClient routeclientset.RouteV1Interface
+	kubeClient  kubernetes.Interface
 }
 
 // NewResources create new webhook resources
@@ -20,11 +23,22 @@ func NewResources() (*webhookResources, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &webhookResources{routeClient: routeClient}, nil
+
+	kubeClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return &webhookResources{routeClient: routeClient,
+		kubeClient: kubeClient}, nil
 }
 
-func (r *webhookResources) getWebhookSecret(ns, secetName string) (string, error) {
-	return "", nil
+func (r *webhookResources) getWebhookSecret(ns, secetName, key string) (string, error) {
+
+	secret, err := r.kubeClient.CoreV1().Secrets(ns).Get(secetName, metav1.GetOptions{})
+	if err != nil {
+		return "", errors.Wrapf(err, "unable to get the secret %s", secret)
+	}
+	return string(secret.Data[key]), nil
 }
 
 func (r *webhookResources) getListenerAddress(ns, routeName string) (string, error) {
--- pkg/manifest/webhook/webhook_resources_test.go
+++ pkg/manifest/webhook/webhook_resources_test.go
@@ -20,5 +20,21 @@ func TestGetRouteHost(t *testing.T) {
 	if diff := cmp.Diff(route, "gitops-webhook-event-listener-route-tst-cicd.apps.gitops1.devcluster.openshift.com"); diff != "" {
 		t.Errorf("driver errMsg mismatch got\n%s", diff)
 	}
+}
+
+func TestGetSecret(t *testing.T) {
+	resources, err := NewResources()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	secret, err := resources.getWebhookSecret("tst-cicd", "gitops-webhook-secret", "webhook-secret-key")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if diff := cmp.Diff(secret, "testing"); diff != "" {
+		t.Errorf("driver errMsg mismatch got\n%s", diff)
+	}
 
 }
