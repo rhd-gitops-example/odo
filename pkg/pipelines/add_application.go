ea87cdb9f9b00b1c69e5167c9c18ed6dd202cd9e
Gagan Hegde <ghegde@redhat.com>
2020-05-04 14:07:22 -0400

    Modularized the AddServices, checked for special conditions

--- pkg/pipelines/add_application.go
+++ pkg/pipelines/add_application.go
@@ -2,14 +2,16 @@ package pipelines
 
 import (
 	"fmt"
-	"log"
+	"path/filepath"
 
 	"github.com/openshift/odo/pkg/pipelines/config"
-	"github.com/openshift/odo/pkg/pipelines/yaml"
-
+	"github.com/openshift/odo/pkg/pipelines/eventlisteners"
+	"github.com/openshift/odo/pkg/pipelines/meta"
 	"github.com/openshift/odo/pkg/pipelines/resources"
-
+	"github.com/openshift/odo/pkg/pipelines/secrets"
+	"github.com/openshift/odo/pkg/pipelines/yaml"
 	"github.com/spf13/afero"
+	"sigs.k8s.io/kustomize/pkg/types"
 )
 
 // EnvParameters encapsulates parameters for add env command
@@ -27,35 +29,50 @@ const filename = "pipelines.yaml"
 // Env will bootstrap a new environment directory
 func AddService(o *ServiceParameters, fs afero.Fs) error {
 
+	repoName, err := repoFromURL(o.ServiceGitRepo)
+	ns := NamespaceNames("")
+	secretName := "github-webhook-secret-" + repoName + "-svc"
+
+	secretFileName := filepath.Join("03-secrets", secretName+".yaml")
+	secretsPath := filepath.Join("environments", ns["cicd"], "base", "pipelines", secretFileName)
+	secretsKustomisePath := filepath.Join("environments", o.EnvName, "services", repoName, "base", "kustomization.yaml")
+
+	files := resources.Resources{}
+
 	m, err := config.ParseFile(fs, o.Manifest)
 	if err != nil {
 		return fmt.Errorf("failed to parse manifest: %w", err)
 	}
+
 	env, err := m.GetEnvironment(o.EnvName)
 	if env == nil {
-		return fmt.Errorf("environment does not exist already", o.EnvName)
+		return fmt.Errorf("environment does not exist already at %s", o.EnvName)
 	}
 
-	repoName, err := repoFromURL(o.ServiceGitRepo)
-
-	secretName := "github-webhook-secret-" + repoName + "-svc"
-
 	app, err := m.GetApplication(o.EnvName, o.AppName)
-
+	if err != nil {
+		return err
+	}
 	if app == nil {
-		Newapp, _ := ApplicationFromRepo(o.AppName, o.ServiceGitRepo, secretName, "cicd")
+		Newapp, _ := ApplicationFromRepo(o.AppName, o.ServiceGitRepo, secretName, ns["cicd"])
 		env.Apps = append(env.Apps, Newapp)
-		log.Println("was here in the if")
 	} else {
-		log.Println("Was here in the else")
-		service := GetService(repoName, o.ServiceGitRepo, secretName, "cicd")
+		err := checkServiceExists(app.Services, repoName)
+		if err != nil {
+			return err
+		}
+		service := GetService(repoName, o.ServiceGitRepo, secretName, ns["cicd"])
 		app.Services = append(app.Services, service)
 		env.Apps = []*config.Application{app}
 	}
 
-	files := resources.Resources{}
+	hookSecret, err := secrets.CreateSealedSecret(
+		meta.NamespacedName(ns["cicd"], secretName),
+		o.ServiceWebhookSecret,
+		eventlisteners.WebhookSecretKey)
 
 	files[o.Manifest] = m
+	files[secretsPath] = hookSecret
 
 	buildParams := &BuildParameters{
 		ManifestFilename: o.Manifest,
@@ -70,6 +87,29 @@ func AddService(o *ServiceParameters, fs afero.Fs) error {
 	}
 	files = resources.Merge(built, files)
 
+	kustFiles := appKustomise([]string{secretsPath}, []string{"./config"})
+
+	files[secretsKustomisePath] = kustFiles
+
 	_, err = yaml.WriteResources(fs, o.Output, files)
-	return err
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func appKustomise(bases, resources []string) *types.Kustomization {
+	return &types.Kustomization{
+		Resources: bases,
+		Bases:     resources,
+	}
+}
+
+func checkServiceExists(services []*config.Service, serviceName string) error {
+	for _, service := range services {
+		if service.Name == serviceName {
+			return fmt.Errorf("A Service with this name %s already exists", serviceName)
+		}
+	}
+	return nil
 }
