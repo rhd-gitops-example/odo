22d063b58452bf69c0f9ed16310df34d44cac063
ishitasequeira <isequeir@redhat.com>
2020-05-06 02:41:11 -0400

    added test case for cicd environment

--- pkg/pipelines/add_service_test.go
+++ pkg/pipelines/add_service_test.go
@@ -36,3 +36,32 @@ func TestServiceWithoutCICD(t *testing.T) {
 		})
 	}
 }
+
+func TestServiceWithCICD(t *testing.T) {
+
+	fakeFs := ioutils.NewMapFilesystem()
+	gitopsPath := afero.GetTempDir(fakeFs, "test")
+
+	manifestFile := filepath.Join(gitopsPath, pipelinesFile)
+	kustomization := filepath.Join(gitopsPath, "environments/cicd/base/pipelines/kustomization.yaml")
+	afero.WriteFile(fakeFs, kustomization, []byte(""), 0644)
+	afero.WriteFile(fakeFs, manifestFile, []byte("environments:\n - name: test\n - cicd: true\n   name: cicd\n"), 0644)
+	if err := AddService(testSvcRepo, "123", "test", "app1", manifestFile, fakeFs); err != nil {
+		t.Fatalf("AddService() failed :%s", err)
+	}
+
+	wantedPaths := []string{
+		"environments/test/apps/app1/base/kustomization.yaml",
+		"environments/test/apps/app1/kustomization.yaml",
+		"environments/test/apps/app1/overlays/kustomization.yaml",
+		"environments/test/services/http-api/base/kustomization.yaml",
+		"environments/test/services/http-api/kustomization.yaml",
+		"environments/test/services/http-api/overlays/kustomization.yaml",
+	}
+
+	for _, path := range wantedPaths {
+		t.Run(fmt.Sprintf("checking path %s already exists", path), func(rt *testing.T) {
+			assertFileExists(rt, fakeFs, filepath.Join(gitopsPath, path))
+		})
+	}
+}
