e8e5ab4501750b5c2d221fbf446d2120d8503e92
William Tam <email.wtam@gmail.com>
2020-05-17 17:50:20 -0400

    Fix tests

--- pkg/pipelines/triggers/bindings_test.go
+++ pkg/pipelines/triggers/bindings_test.go
@@ -1,6 +1,7 @@
 package triggers
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -109,26 +110,84 @@ func TestCreateBindingParam(t *testing.T) {
 }
 
 func TestCreateImageRepoBinding(t *testing.T) {
-	imageRepoBinding := triggersv1.TriggerBinding{
-		TypeMeta: triggerBindingTypeMeta,
-		ObjectMeta: v1.ObjectMeta{
-			Name:      "test-binding",
-			Namespace: "testns",
+
+	testcases := []struct {
+		ns        string
+		name      string
+		imageRepo string
+		tlsVerify string
+		want      triggersv1.TriggerBinding
+	}{
+		{
+			ns:        "testns",
+			name:      "test-binding",
+			imageRepo: "quay.io/user/testing",
+			tlsVerify: "true",
+			want: triggersv1.TriggerBinding{
+				TypeMeta: triggerBindingTypeMeta,
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test-binding",
+					Namespace: "testns",
+				},
+				Spec: triggersv1.TriggerBindingSpec{
+					Params: []pipelinev1.Param{
+						{
+							Name: "imageRepo",
+							Value: pipelinev1.ArrayOrString{
+								StringVal: "quay.io/user/testing",
+								Type:      pipelinev1.ParamTypeString,
+							},
+						},
+						{
+							Name: "tlsVerify",
+							Value: pipelinev1.ArrayOrString{
+								StringVal: "true",
+								Type:      pipelinev1.ParamTypeString,
+							},
+						},
+					},
+				},
+			},
 		},
-		Spec: triggersv1.TriggerBindingSpec{
-			Params: []pipelinev1.Param{
-				{
-					Name: "imageRepo",
-					Value: pipelinev1.ArrayOrString{
-						StringVal: "quay.io/user/testing",
-						Type:      pipelinev1.ParamTypeString,
+		{
+			ns:        "testns",
+			name:      "test-binding",
+			imageRepo: "proj/taxi",
+			tlsVerify: "false",
+			want: triggersv1.TriggerBinding{
+				TypeMeta: triggerBindingTypeMeta,
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test-binding",
+					Namespace: "testns",
+				},
+				Spec: triggersv1.TriggerBindingSpec{
+					Params: []pipelinev1.Param{
+						{
+							Name: "imageRepo",
+							Value: pipelinev1.ArrayOrString{
+								StringVal: "proj/taxi",
+								Type:      pipelinev1.ParamTypeString,
+							},
+						},
+						{
+							Name: "tlsVerify",
+							Value: pipelinev1.ArrayOrString{
+								StringVal: "false",
+								Type:      pipelinev1.ParamTypeString,
+							},
+						},
 					},
 				},
 			},
 		},
 	}
-	binding := CreateImageRepoBinding("testns", "test-binding", "quay.io/user/testing")
-	if diff := cmp.Diff(imageRepoBinding, binding); diff != "" {
-		t.Fatalf("CreateImageRepoBinding() failed:\n%s", diff)
+
+	for i, tt := range testcases {
+		t.Run(fmt.Sprintf("Test %d", i), func(t *testing.T) {
+			got := CreateImageRepoBinding(tt.ns, tt.name, tt.imageRepo, tt.tlsVerify)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Fatalf("CreateImageRepoBinding() failed:\n%s", diff)
+			}
+		})
 	}
 }
