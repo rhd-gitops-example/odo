2f7e9e58500178fda9b2024ddee3edc728fa8a24
Kevin McDermott <bigkevmcd@gmail.com>
2020-06-10 09:57:36 +0100

    Add support for detecting failures when executing.
     - When the exit code is > 0 for a command, then exit at the end with the code.
     - Renames "Client" to "Command"
     - Adds t.Helper to improve test failures

--- pkg/pipelines/routes/routes_test.go
+++ pkg/pipelines/routes/routes_test.go
@@ -92,7 +92,6 @@ func TestMarshalRoute(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	log.Printf("KEVIN!!!! %s\n", b)
 	if strings.Index(strings.ToLower(string(b)), "ingress") > -1 {
 		t.Fatal("the marshaled output contains the ingress field")
 	}
