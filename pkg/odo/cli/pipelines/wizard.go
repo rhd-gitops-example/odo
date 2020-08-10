c09b2598b8fe7a19ddc1f46deca1ccf081f1282a
ishitasequeira <isequeir@redhat.com>
2020-08-10 15:38:48 -0400

    added condition yo check if interactive or not

--- pkg/odo/cli/pipelines/wizard.go
+++ pkg/odo/cli/pipelines/wizard.go
@@ -47,6 +47,7 @@ var (
 
 // WizardParameters encapsulates the parameters for the odo pipelines init command.
 type WizardParameters struct {
+	interactive bool
 	*pipelines.BootstrapOptions
 	// generic context options common to all commands
 	*genericclioptions.Context
@@ -79,46 +80,49 @@ func (io *WizardParameters) Complete(name string, cmd *cobra.Command, args []str
 	if err != nil {
 		return err
 	}
-
-	// ask for sealed secrets only when default is absent
-	if io.SealedSecretsService == (types.NamespacedName{}) {
-		io.SealedSecretsService.Name = ui.EnterSealedSecretService(&io.SealedSecretsService)
-
-	}
-
-	io.GitOpsRepoURL = ui.EnterGitRepo()
-	io.GitOpsRepoURL = utility.AddGitSuffixIfNecessary(io.GitOpsRepoURL)
-	_, err = sc.NewRepository(io.GitOpsRepoURL)
-	if err != nil {
-		return err
-	}
-	option := ui.SelectOptionImageRepository()
-	if option == "Openshift Internal repository" {
-		io.InternalRegistryHostname = ui.EnterInternalRegistry()
-		io.ImageRepo = ui.EnterImageRepoInternalRegistry()
+	if io.GitOpsRepoURL == "" || len(args) > 0 || cmd.HasFlags() {
 
 	} else {
-		io.DockerConfigJSONFilename = ui.EnterDockercfg()
-		fs := ioutils.NewFilesystem()
-		_, err := pipelines.CheckFileExists(fs, io.DockerConfigJSONFilename)
+		// ask for sealed secrets only when default is absent
+		if io.SealedSecretsService == (types.NamespacedName{}) {
+			io.SealedSecretsService.Name = ui.EnterSealedSecretService(&io.SealedSecretsService)
+
+		}
+
+		io.GitOpsRepoURL = ui.EnterGitRepo()
+		io.GitOpsRepoURL = utility.AddGitSuffixIfNecessary(io.GitOpsRepoURL)
+		_, err = sc.NewRepository(io.GitOpsRepoURL)
 		if err != nil {
 			return err
 		}
-		io.ImageRepo = ui.EnterImageRepoExternalRepository()
+		option := ui.SelectOptionImageRepository()
+		if option == "Openshift Internal repository" {
+			io.InternalRegistryHostname = ui.EnterInternalRegistry()
+			io.ImageRepo = ui.EnterImageRepoInternalRegistry()
+
+		} else {
+			io.DockerConfigJSONFilename = ui.EnterDockercfg()
+			fs := ioutils.NewFilesystem()
+			_, err := pipelines.CheckFileExists(fs, io.DockerConfigJSONFilename)
+			if err != nil {
+				return err
+			}
+			io.ImageRepo = ui.EnterImageRepoExternalRepository()
+		}
+		io.GitOpsWebhookSecret = ui.EnterGitWebhookSecret()
+		io.Prefix = ui.EnterPrefix()
+		io.ServiceRepoURL = ui.EnterServiceRepoURL()
+		io.Prefix = utility.MaybeCompletePrefix(io.Prefix)
+		io.ServiceRepoURL = utility.AddGitSuffixIfNecessary(io.ServiceRepoURL)
+		io.ServiceWebhookSecret = ui.EnterServiceWebhookSecret()
+		commitStatusTrackerCheck := ui.SelectOptionCommitStatusTracker()
+		if commitStatusTrackerCheck == "yes" {
+			io.StatusTrackerAccessToken = ui.EnterStatusTrackerAccessToken(io.ServiceRepoURL)
+		}
+		io.OutputPath = ui.EnterOutputPath()
+		io.Overwrite = true
+		return nil
 	}
-	io.GitOpsWebhookSecret = ui.EnterGitWebhookSecret()
-	io.Prefix = ui.EnterPrefix()
-	io.ServiceRepoURL = ui.EnterServiceRepoURL()
-	io.Prefix = utility.MaybeCompletePrefix(io.Prefix)
-	io.ServiceRepoURL = utility.AddGitSuffixIfNecessary(io.ServiceRepoURL)
-	io.ServiceWebhookSecret = ui.EnterServiceWebhookSecret()
-	commitStatusTrackerCheck := ui.SelectOptionCommitStatusTracker()
-	if commitStatusTrackerCheck == "yes" {
-		io.StatusTrackerAccessToken = ui.EnterStatusTrackerAccessToken(io.ServiceRepoURL)
-	}
-	io.OutputPath = ui.EnterOutputPath()
-	io.Overwrite = true
-	return nil
 }
 
 func checkBootstrapDependencies(io *WizardParameters, kubeClient kubernetes.Interface, spinner status) error {
@@ -212,10 +216,19 @@ func NewCmdWizard(name, fullName string) *cobra.Command {
 			genericclioptions.GenericRun(o, cmd, args)
 		},
 	}
-	addInitCommands(wizardCmd, o.BootstrapOptions.InitOptions)
 	wizardCmd.Flags().StringVar(&o.ServiceRepoURL, "service-repo-url", "", "Provide the URL for your Service repository e.g. https://github.com/organisation/service.git")
 	wizardCmd.Flags().StringVar(&o.ServiceWebhookSecret, "service-webhook-secret", "", "Provide a secret that we can use to authenticate incoming hooks from your Git hosting service for the Service repository. (if not provided, it will be auto-generated)")
-	// wizardCmd.MarkFlagRequired("gitops-repo-url")
+	wizardCmd.Flags().StringVar(&o.GitOpsRepoURL, "gitops-repo-url", "", "Provide the URL for your GitOps repository e.g. https://github.com/organisation/repository.git")
+	wizardCmd.Flags().StringVar(&o.GitOpsWebhookSecret, "gitops-webhook-secret", "", "Provide a secret that we can use to authenticate incoming hooks from your Git hosting service for the GitOps repository. (if not provided, it will be auto-generated)")
+	wizardCmd.Flags().StringVar(&o.OutputPath, "output", ".", "Path to write GitOps resources")
+	wizardCmd.Flags().StringVarP(&o.Prefix, "prefix", "p", "", "Add a prefix to the environment names(Dev, stage,prod,cicd etc.) to distinguish and identify individual environments")
+	wizardCmd.Flags().StringVar(&o.DockerConfigJSONFilename, "dockercfgjson", "~/.docker/config.json", "Filepath to config.json which authenticates the image push to the desired image registry ")
+	wizardCmd.Flags().StringVar(&o.InternalRegistryHostname, "image-repo-internal-registry-hostname", "image-registry.openshift-image-registry.svc:5000", "Host-name for internal image registry e.g. docker-registry.default.svc.cluster.local:5000, used if you are pushing your images to the internal image registry")
+	wizardCmd.Flags().StringVar(&o.ImageRepo, "image-repo", "", "Image repository of the form <registry>/<username>/<repository> or <project>/<app> which is used to push newly built images")
+	wizardCmd.Flags().StringVar(&o.SealedSecretsService.Namespace, "sealed-secrets-ns", "kube-system", "Namespace in which the Sealed Secrets operator is installed, automatically generated secrets are encrypted with this operator")
+	wizardCmd.Flags().StringVar(&o.SealedSecretsService.Name, "sealed-secrets-svc", "sealed-secrets-controller", "Name of the Sealed Secrets Services that encrypts secrets")
+	wizardCmd.Flags().StringVar(&o.StatusTrackerAccessToken, "status-tracker-access-token", "", "Used to authenticate requests to push commit-statuses to your Git hosting service")
+	wizardCmd.Flags().BoolVar(&o.Overwrite, "overwrite", false, "Overwrites previously existing GitOps configuration (if any)")
 
 	return wizardCmd
 }
